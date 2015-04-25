94fedf8d9a254de4f2a9609edffa912410cb05c1
lucasSimonelli <lucasp.simonelli@gmail.com>
2015-04-25 10:52:51 -0300

    Applied champion's method to create processes

--- main.cpp
+++ main.cpp
@@ -7,30 +7,30 @@
 
 list<Order> createOrders();
 
-void createReceptionists(int index, int amount);
+int createReceptionists(int amount);
 
 using namespace std;
 
 int main() {
     Logger::logger().log("Launching app...");
     list<Order> orders = createOrders();
-    createReceptionists(0, 2);
+    int resultReceptionist = createReceptionists(2);
+    if (resultReceptionist == 0) return 0;
     Logger::logger().log("Exiting app");
     return 0;
 }
 
-void createReceptionists(int index, int amount) {
-    if (index < amount) {
+int createReceptionists(int amount) {
+    for (int i = 0; i < amount; i++) {
         int pid = fork();
         if (pid == 0) {
             Receptionist();
-        } else {
-            //Father
-            createReceptionists(index + 1, amount);
+            return 0;
         }
     }
-}
 
+    return 1;
+}
 list<Order> createOrders() {
     return {Order()};
 }
