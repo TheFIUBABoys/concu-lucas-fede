e10e3b7179d3e19b9f9424f9722fb5ac091079ea
lucasSimonelli <lucasp.simonelli@gmail.com>
2015-04-25 10:58:21 -0300

    Small changes

--- main.cpp
+++ main.cpp
@@ -5,9 +5,14 @@
 #include "src/Domain/Order/Order.h"
 #include "src/Domain/Receptionist/Receptionist.h"
 
+typedef enum ProcessType{
+    ProcessTypeFather,
+    ProcessTypeChild
+};
+
 list<Order> createOrders();
 
-int createReceptionists(int amount);
+ProcessType createReceptionists(int amount);
 
 using namespace std;
 
@@ -16,22 +21,22 @@ int main() {
     list<Order> orders = createOrders();
 
     int resultReceptionist = createReceptionists(2);
-    if (resultReceptionist == 0) return 0;
+    if (resultReceptionist == ProcessTypeChild) return 0;
 
     Logger::logger().log("Exiting app");
     return 0;
 }
 
-int createReceptionists(int amount) {
+ProcessType createReceptionists(int amount) {
     for (int i = 0; i < amount; i++) {
         int pid = fork();
         if (pid == 0) {
             Receptionist();
-            return 0;
+            return ProcessTypeChild;
         }
     }
 
-    return 1;
+    return ProcessTypeFather;
 }
 list<Order> createOrders() {
     return {Order()};
--- src/Domain/Receptionist/Receptionist.cpp
+++ src/Domain/Receptionist/Receptionist.cpp
@@ -2,23 +2,25 @@
 // Created by lucas on 4/20/15.
 //
 
-#include <unistd.h>
 #include "Receptionist.h"
 #include "../../Util/Logger/Logger.h"
 
 
 //Create receptionist in new thread and start polling for orders
 Receptionist::Receptionist() {
+    Logger::logger().log("Receptionist waking up");
     startPollingForOrders();
+    Logger::logger().log("Receptionist dying");
 }
 
 void Receptionist::startPollingForOrders() {
     int i = 0;
     while (true) {
         if (i > 10) break;
-        Logger::logger().log("Polling for resources");
+        Logger::logger().log("Waiting for orders ");
         i++;
     }
+
 }
 
 /*int main() {
